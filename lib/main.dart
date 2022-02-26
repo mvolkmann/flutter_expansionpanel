01929e796a68001c4c84752fd4baee86d4a7d366
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2022-02-26 07:17:14 -0600

    progress

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import './extensions/widget_extensions.dart';
 
 const title = 'My App';
 
@@ -36,6 +37,9 @@ class _HomeState extends State<Home> {
         child: SingleChildScrollView(
           child: ExpansionPanelList(
             children: panels,
+            dividerColor: Colors.purple,
+            elevation: 4, // max is 4
+            expandedHeaderPadding: EdgeInsets.all(0), // only when expanded
             expansionCallback: (index, isOpen) {
               setState(() => panelOpen[index] = !isOpen);
             },
@@ -47,11 +51,14 @@ class _HomeState extends State<Home> {
 
   ExpansionPanel buildPanel(int index, String header, String body) =>
       ExpansionPanel(
+        backgroundColor: Colors.yellow[100],
+        canTapOnHeader: true,
         headerBuilder: (_, isOpen) {
           final color = isOpen ? Colors.green : Colors.red;
-          return Text(header, style: TextStyle(color: color));
+          return Text(header, style: TextStyle(color: color))
+              .paddingOnly(left: 15, top: 15);
         },
-        body: Text(body),
+        body: Text(body).paddingOnly(top: 0, bottom: 15),
         isExpanded: panelOpen[index],
       );
 }
